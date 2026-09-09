b65e122d307540cbb8df1bc497b42cf3b918062d
Asia-Arif <asiarajpoot9320@gmail.com>
2026-09-09 20:57:30 +0500

    Setup realtime chat app backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,14 @@
 // const express=require("express")
 import express from "express"
+import User from "./models/user.model.js"
 import "dotenv/config"
+import { connectDB } from "./lib/db.js"
 const app=express()
 const PORT=process.env.PORT
-app.listen(PORT,()=>console.log("server is running on  PORT:",PORT))
+app.get("/health",(req,res)=>{
+    res.status(200).json({ok:true})
+})
+app.listen(PORT,()=>{
+    connectDB()
+    console.log("server is running on  PORT:",PORT)
+})
