4e0291fc3306a676b25bad76b8869d08daf79085
Asia-Arif <asiarajpoot9320@gmail.com>
2026-09-10 19:43:25 +0500

    Prepare iMessage for Railway deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,13 +2,19 @@
 import express, { json } from "express"
 import cors from "cors"
 import "dotenv/config"
+
+import fs from "fs"
+import path from "path"
 import User from "./models/user.model.js"
 import { clerkMiddleware } from '@clerk/express'
-import { connectDB } from "./lib/db.js"
+import { connectDB } from "./lib/db.js";
+import job from "./cron.js";
 const app=express()
 const PORT=process.env.PORT
 
 const FRONTEND_URL=process.env.FRONTEND_URL
+
+const publicDir=path.join(process.cwd(),"public")
 app.use(express.json())
 app.use(cors({origin:FRONTEND_URL,credentials:true}))
 app.use(clerkMiddleware())
@@ -16,7 +22,18 @@ app.get("/health",(req,res)=>{
     // const {message,image,video}=req.body
     res.status(200).json({ok:true})
 })
+// if the public directory exist ,serve the static file
+//this is for the production bulid
+if(fs.existsSync(publicDir)){
+    app.use(express.static(publicDir))
+
+    app.get("/{*any}",(req,res,next)=>{
+        res.sendFile(path.join(publicDir,"index.html"),(err)=>next(err))
+    })
+}
 app.listen(PORT,()=>{
     connectDB()
-    console.log("server is running on  PORT:",PORT)
+    console.log("server is running on  PORT:",PORT);
+    if(process.env.NODE_ENV==="production") job.start()
+    
 })
