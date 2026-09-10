eccce123d8a840080b818e7b39871d663a0e90d6
Asia-Arif <asiarajpoot9320@gmail.com>
2026-09-10 20:29:44 +0500

    Fix cron import path

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,7 +8,7 @@ import path from "path"
 import User from "./models/user.model.js"
 import { clerkMiddleware } from '@clerk/express'
 import { connectDB } from "./lib/db.js";
-import job from "./cron.js";
+import job from "./lib/cron.js";
 const app=express()
 const PORT=process.env.PORT
 
