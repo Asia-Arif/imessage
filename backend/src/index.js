6ca0d49f412eb82bc0f796e1515a336a1e6a01b3
Asia-Arif <asiarajpoot9320@gmail.com>
2026-09-10 13:24:21 +0500

    Initial backend setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,19 @@
 // const express=require("express")
-import express from "express"
-import User from "./models/user.model.js"
+import express, { json } from "express"
+import cors from "cors"
 import "dotenv/config"
+import User from "./models/user.model.js"
+import { clerkMiddleware } from '@clerk/express'
 import { connectDB } from "./lib/db.js"
 const app=express()
 const PORT=process.env.PORT
+
+const FRONTEND_URL=process.env.FRONTEND_URL
+app.use(express.json())
+app.use(cors({origin:FRONTEND_URL,credentials:true}))
+app.use(clerkMiddleware())
 app.get("/health",(req,res)=>{
+    // const {message,image,video}=req.body
     res.status(200).json({ok:true})
 })
 app.listen(PORT,()=>{
